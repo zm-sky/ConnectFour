b18b5ecec42f3107d41931fc938ae5902f35fca6
zmsky <hiresky3201@gmail.com>
2018-02-03 01:18:53 -0700

    create board and boardUI classes.

--- src/me/pokemutt/connectFour/game/Board.java
+++ src/me/pokemutt/connectFour/game/Board.java
@@ -6,5 +6,21 @@ package me.pokemutt.connectFour.game;
  *
  */
 public class Board {
-
+	
+	//Representa el numero de celdas horizontales.
+	private int hcells;
+	//Representa el numero de celdas verticales.
+	private int vcells;
+	
+	/**
+	 * Crea una instancia vacia de un tablero. Este guarda datos,
+	 * ofrece metodos para la manipulacion de los mismos entre otras utilidades.
+	 * Para visualizar este tablero, se puede utilizar la clase BoardUI.
+	 * 
+	 * @param hcells La cantidad de celdas horizontales.
+	 * @param vcells La cantidad de celdas verticales.
+	 */
+	public Board(int hcells, int vcells){
+		
+	}
 }
---
+++ src/me/pokemutt/connectFour/ui/BoardUI.java
@@ -0,0 +1,11 @@
+package me.pokemutt.connectFour.ui;
+
+/**
+ * 
+ * @author Raul Karim Sabag Ballesteros
+ *
+ */
+public class BoardUI {
+
+	
+}
